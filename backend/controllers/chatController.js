fdd2e35f431b35ab5dc34b46961d6beaf681610f
soumick727 <roysoumick727@gmail.com>
2025-06-25 00:27:47 +0530

    changes made

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -67,14 +67,193 @@ exports.fetchChats = async (req, res) => {
 }
 
 exports.createGroupChat = async (req, res) => {
+    try {
+    const {chatName, members} = req.body;
+    if(!chatName || !members || members.length <2){
+        return res.status(404).json({
+            message: "Chat name and at least two members are required",
+            success: false
+        })
+    }
+    // check if same member is added multiple times
+    const uniqueMembers = [...new Set([...members, req.user._id.toString()])];
+    if(!uniqueMembers || uniqueMembers.length < 2){
+        return res.status(400).json({
+            message: "At least two unique members are required",
+            success: false
+        })
+    }
+    // add the current user to the member list
+    members.push(req.user._id);
 
+    // create a new group chat
+     const groupChat = new Chat({
+        chatName,
+        isGroupChat: true,
+        members: uniqueMembers,
+        groupAdmin: req.user._id
+     })
+
+     const savedGroupChat = await groupChat.save();
+
+     res.status(201).json({
+        message: "group chat created successfully",
+        success: true,
+        data: savedGroupChat
+     })
+    
+    } catch (error) {
+        res.status(500).json({
+            message: "Error creating group chat",
+            success: false,
+            error: error.message
+        })
+    }
+    
 }
 exports.renameGroup = async (req, res) => {
-    
+   try {
+        const {chatId, newChatName} = req.body;
+        if(!chatId || !newChatName){
+            return res.status(400).json({
+                message: "Chat ID and new chat name are required",
+                success: false
+            })
+        }
+
+        //find the group chat by id
+        const chat = await Chat.findById(chatId);
+        if(!chat || !chat.isGroupChat){
+            return res.status(404).json({
+                message: "Group chat not found",
+                success: false
+            });
+        }
+        //check if the user is the group admin
+        if(chat.groupAdmin.toString() !== req.user._id.toString()){
+            return res.status(403).json({
+                message: "You are not authorized to rename this group",
+                success: false
+            });
+        }
+
+        // rename the group chat
+        chat.chatName = newChatName;
+        const updatedChatName = await chat.save();
+        res.status(200).json({
+            message: "Group chat renamed successfully",
+            success: true,
+            data: updatedChatName
+        });
+   } catch (error) {
+       res.status(500).json({
+           message: "Error renaming group",
+           success: false,
+           error: error.message
+       });
+   }
 }
 exports.addToGroup = async (req, res) => {
-    
+    try {
+        const {chatId, userIdToAdd} =  req.body;
+        if(!chatId || !userIdToAdd){
+            return res.status(400).json({
+                message: "Chat Id and user id to add are required",
+                success:false
+            })
+        }
+        //find the group chat
+        const chat = await Chat.findById(chatId);
+        if(!chat || !chat.isGroupChat){
+            return res.status(404).json({
+                message: "Group chat not found",
+                success: false
+            });
+        }
+
+        // check if the user is the group admin or not
+        if(chat.groupAdmin.toString() !== req.user._id.toString()){
+            return res.status(400).json({
+                message: "you are not authorized to add members to this group",
+                success: false
+            })
+        }
+
+        // check if the user is already a member of the group
+        if(chat.members.includes(userIdToAdd)){
+            return res.status(400).json({
+                message: "user id is already present in the group",
+                success: false
+            })
+        }
+
+        // add the user to the group
+        chat.members.push(userIdToAdd)
+        const updatedChat = await chat.save();
+
+        res.status(200).json({
+            message: "User added to group successfully",
+            success: true,
+            data: updatedChat
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Error adding to group",
+            success: false,
+            error: error.message
+        });
+        
+    }
 }
 exports.removeFromGroup = async (req, res) => {
-    
+    try {
+        const {chatId, userIdToRemove} = req.body;
+        if(!chatId || !userIdToRemove){
+            return res.status(400).json({
+                message: "Chat Id and user id to remove are required",
+                success: false
+            })
+        }
+        //find the group chat
+        const chat = await Chat.findById(chatId);
+        if(!chat || !chat.isGroupChat){
+            return res.status(404).json({
+                message: "Group chat not found",
+                success: false
+            });
+        }
+
+        // check if the user is the group admin or not
+        if(chat.groupAdmin.toString() !== req.user._id.toString()){
+            return res.status(400).json({
+                message: "you are not authorized to remove members from this group",
+                success: false
+            })
+        }
+
+        // check if the user is a member of the group
+        if(!chat.members.includes(userIdToRemove)){
+            return res.status(400).json({
+                message: "user id is not present in the group",
+                success: false
+            })
+        }
+
+        // remove the user from the group
+        chat.members = chat.members.filter(member => member.toString() !== userIdToRemove);
+        const updatedChat = await chat.save();
+
+        res.status(200).json({
+            message: "User removed from group successfully",
+            success: true,
+            data: updatedChat
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Error removing from group",
+            success: false,
+            error: error.message
+        });
+        
+    }
 }
